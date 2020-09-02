ca6ec0b90acd9f7157c4d1839eaf5a43962acd6e
NA Flipper <dioncleung@gmail.com>
2020-09-01 17:39:15 -0700

    changed responsiveness for navbar. added burger icon

--- src/components/FirstHeader.js
+++ src/components/FirstHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FirstHeader(props) {
   return (
-    <h1 className="border border-red-700 border-4 text-center text-5xl bg-white sticky top-0 pt-24">
+    <h1 className="border border-red-700 border-4 text-center lg:text-5xl text-2xl bg-white sticky top-0 lg:pt-24 pt-12">
       {props.text}
     </h1>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,33 +2,40 @@ import React from "react";
 // import { BrowserRouter as Link } from "react-router-dom";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import hamburger from "./photos/hamburger.png";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-around border-solid border-2 border-black w-full pt-8 pb-4 fixed top-0 mb-10 bg-opacity-50 bg-yellow-700 z-20">
-      <AnchorLink href="#top" className="text-4xl">
+    <div className="flex justify-around border-solid border-2 border-black w-full lg:pt-8 lg:pb-4 fixed top-0 mb-10 bg-teal-300 z-20">
+      <AnchorLink href="#top" className="lg:text-4xl text-2xl">
         Welcome
       </AnchorLink>
-      <AnchorLink href="#about-me" className="text-lg">
+      <AnchorLink href="#about-me" className="hidden lg:inline-block text-lg">
         about me
       </AnchorLink>
-      <AnchorLink href="#meet-me" className="text-lg">
+      <AnchorLink href="#meet-me" className="hidden lg:inline-block text-lg">
         meet me
       </AnchorLink>
-      <AnchorLink href="#qualifications" className="text-lg">
+      <AnchorLink
+        href="#qualifications"
+        className="hidden lg:inline-block text-lg"
+      >
         qualifications
       </AnchorLink>
-      <AnchorLink href="#portfolio" className="text-lg">
+      <AnchorLink href="#portfolio" className="hidden lg:inline-block text-lg">
         portfolio
       </AnchorLink>
-      <AnchorLink href="#testimonials" className="text-lg">
+      <AnchorLink
+        href="#testimonials"
+        className="hidden lg:inline-block text-lg"
+      >
         testimonials
       </AnchorLink>
-      <AnchorLink href="#contact" className="text-lg">
+      <AnchorLink href="#contact" className="hidden lg:inline-block text-lg">
         contact
       </AnchorLink>
       <div>
-        <h1 className="text-lg">Hamburger</h1>
+        <img src={hamburger} alt="" className="lg:hidden w-12" />
       </div>
     </div>
   );
