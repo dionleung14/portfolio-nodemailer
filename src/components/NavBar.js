822dfb707ddb594fd947059b9e03d1ec7518ad32
NA Flipper <dioncleung@gmail.com>
2020-09-02 15:27:10 -0700

    general styling wip

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,37 +6,54 @@ import hamburger from "./photos/hamburger.png";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-around border-solid border-2 border-black w-full lg:pt-8 lg:pb-4 fixed top-0 mb-10 bg-teal-300 z-20">
-      <AnchorLink href="#top" className="lg:text-4xl text-2xl">
+    <div className="flex justify-around border-solid border-2 border-black w-full lg:pt-4 lg:pb-4 fixed top-0 mb-10 bg-teal-300 z-20">
+      <AnchorLink
+        href="#top"
+        className="lg:text-4xl text-2xl border border-transparent hover:border-blue-600"
+      >
         Welcome
       </AnchorLink>
-      <AnchorLink href="#about-me" className="hidden lg:inline-block text-lg">
+      <AnchorLink
+        href="#about-me"
+        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 hover:text-red-300 text-lg"
+      >
         about me
       </AnchorLink>
-      <AnchorLink href="#meet-me" className="hidden lg:inline-block text-lg">
+      <AnchorLink
+        href="#meet-me"
+        className="hidden lg:inline-block self-center text-lg"
+      >
         meet me
       </AnchorLink>
       <AnchorLink
         href="#qualifications"
-        className="hidden lg:inline-block text-lg"
+        className="hidden lg:inline-block self-center text-lg"
       >
         qualifications
       </AnchorLink>
-      <AnchorLink href="#portfolio" className="hidden lg:inline-block text-lg">
+      <AnchorLink
+        href="#portfolio"
+        className="hidden lg:inline-block self-center text-lg"
+      >
         portfolio
       </AnchorLink>
       <AnchorLink
         href="#testimonials"
-        className="hidden lg:inline-block text-lg"
+        className="hidden lg:inline-block self-center text-lg"
       >
         testimonials
       </AnchorLink>
-      <AnchorLink href="#contact" className="hidden lg:inline-block text-lg">
+      <AnchorLink
+        href="#contact"
+        className="hidden lg:inline-block self-center text-lg"
+      >
         contact
       </AnchorLink>
-      <div>
-        <img src={hamburger} alt="" className="lg:hidden w-12" />
-      </div>
+      <img
+        src={hamburger}
+        alt=""
+        className="lg:hidden self-center inline-block w-6"
+      />
     </div>
   );
 }
