44ee030e2af6e438654b9fd66027d07ea3449cfe
NA Flipper <dioncleung@gmail.com>
2020-09-02 17:16:56 -0700

    navbar hover styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,43 +9,43 @@ export default function Navbar() {
     <div className="flex justify-around border-solid border-2 border-black w-full lg:pt-4 lg:pb-4 fixed top-0 mb-10 bg-teal-300 z-20">
       <AnchorLink
         href="#top"
-        className="lg:text-4xl text-2xl border border-transparent hover:border-blue-600"
+        className="lg:text-4xl text-2xl border border-transparent hover:border-blue-600 px-2 rounded"
       >
         Welcome
       </AnchorLink>
       <AnchorLink
         href="#about-me"
-        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 hover:text-red-300 text-lg"
+        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 px-2 rounded text-lg"
       >
         about me
       </AnchorLink>
       <AnchorLink
         href="#meet-me"
-        className="hidden lg:inline-block self-center text-lg"
+        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 px-2 rounded text-lg"
       >
         meet me
       </AnchorLink>
       <AnchorLink
         href="#qualifications"
-        className="hidden lg:inline-block self-center text-lg"
+        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 px-2 rounded text-lg"
       >
         qualifications
       </AnchorLink>
       <AnchorLink
         href="#portfolio"
-        className="hidden lg:inline-block self-center text-lg"
+        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 px-2 rounded text-lg"
       >
         portfolio
       </AnchorLink>
       <AnchorLink
         href="#testimonials"
-        className="hidden lg:inline-block self-center text-lg"
+        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 px-2 rounded text-lg"
       >
         testimonials
       </AnchorLink>
       <AnchorLink
         href="#contact"
-        className="hidden lg:inline-block self-center text-lg"
+        className="hidden lg:inline-block self-center border border-transparent hover:border-red-700 px-2 rounded text-lg"
       >
         contact
       </AnchorLink>
