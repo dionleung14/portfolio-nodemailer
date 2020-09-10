7e0d2f4acfbea752230ddd96c2e013b9d5d572ef
NA Flipper <dioncleung@gmail.com>
2020-09-10 11:45:22 -0700

    changed color scheme

--- src/components/FirstHeader.js
+++ src/components/FirstHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FirstHeader(props) {
   return (
-    <h1 className="border border-red-700 border-4 text-center lg:text-5xl text-2xl bg-dclnavy-300 text-dclbeige-100 sticky top-0 lg:pt-24 pt-12 font-bold z-20">
+    <h1 className="border border-red-700 border-4 text-center lg:text-5xl text-2xl bg-dclpal1-200 text-white sticky top-0 lg:pt-24 pt-12 font-bold z-20">
       {props.text}
     </h1>
   );
