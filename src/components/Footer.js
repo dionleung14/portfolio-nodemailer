c5714fbe5b1e38ff584df7d03ef28ddf94ac2dd7
NA Flipper <dioncleung@gmail.com>
2020-10-24 03:50:53 -0700

    formatted footer and meet me section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,71 +5,77 @@ export default class Footer extends Component {
   render(props) {
     return (
       <div
-        className={`w-full h-auto lg:h-48 flex justify-around items-center border-t ${
+        className={`w-full h-auto lg:h-48  border-t ${
           this.props.darkModeApp.darkMode
             ? "border-white bg-dclpal1-100 text-white"
             : "border-black"
         }`}
       >
-        <AnchorLink
-          href="#top"
-          className={`border border-transparent px-2 rounded ${
-            this.props.darkModeApp.darkMode
-              ? "hover:border-white"
-              : "hover:border-dclpal1-100"
-          }`}
-        >
-          Back to top
-        </AnchorLink>
-        <a
-          href="https://github.com/dionleung14"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`border border-transparent px-2 rounded ${
-            this.props.darkModeApp.darkMode
-              ? "hover:border-white"
-              : "hover:border-dclpal1-100"
-          }`}
-        >
-          GitHub
-        </a>
-        <a
-          href="https://www.linkedin.com/in/leungdion/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`border border-transparent px-2 rounded ${
-            this.props.darkModeApp.darkMode
-              ? "hover:border-white"
-              : "hover:border-dclpal1-100"
-          }`}
-        >
-          LinkedIn
-        </a>
-        <a
-          href="mailto:dioncleung@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`border border-transparent px-2 rounded ${
-            this.props.darkModeApp.darkMode
-              ? "hover:border-white"
-              : "hover:border-dclpal1-100"
-          }`}
-        >
-          Email
-        </a>
-        <a
-          href="https://www.twitter.com/DionTheDev"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`border border-transparent px-2 rounded ${
-            this.props.darkModeApp.darkMode
-              ? "hover:border-white"
-              : "hover:border-dclpal1-100"
-          }`}
-        >
-          Twitter
-        </a>
-        <div>Copyright &copy; 2020</div>
+        <div className="flex flex-row justify-around my-4">
+          <AnchorLink
+            href="#top"
+            className={`border border-transparent px-2 rounded ${
+              this.props.darkModeApp.darkMode
+                ? "hover:border-white"
+                : "hover:border-dclpal1-100"
+            }`}
+          >
+            Back to top
+          </AnchorLink>
+          <a
+            href="https://github.com/dionleung14"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`border border-transparent px-2 rounded ${
+              this.props.darkModeApp.darkMode
+                ? "hover:border-white"
+                : "hover:border-dclpal1-100"
+            }`}
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/leungdion/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`border border-transparent px-2 rounded ${
+              this.props.darkModeApp.darkMode
+                ? "hover:border-white"
+                : "hover:border-dclpal1-100"
+            }`}
+          >
+            LinkedIn
+          </a>
+          <a
+            href="mailto:dioncleung@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`border border-transparent px-2 rounded ${
+              this.props.darkModeApp.darkMode
+                ? "hover:border-white"
+                : "hover:border-dclpal1-100"
+            }`}
+          >
+            Email
+          </a>
+          <a
+            href="https://www.twitter.com/DionTheDev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`border border-transparent px-2 rounded ${
+              this.props.darkModeApp.darkMode
+                ? "hover:border-white"
+                : "hover:border-dclpal1-100"
+            }`}
+          >
+            Twitter
+          </a>
+          <div>Copyright &copy; 2020</div>
+        </div>
+        <div className="flex flex-row justify-around my-8">
+          Powered by React, Axios, NodeMailer, React Anchor Link Smooth Scroll,
+          and React Router Dom. Styled with Tailwind CSS
+        </div>
       </div>
     );
   }
