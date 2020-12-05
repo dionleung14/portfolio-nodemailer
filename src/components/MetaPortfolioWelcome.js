d054771f30725d34fad32bde36aaf81351625a88
NA Flipper <dioncleung@gmail.com>
2020-12-04 19:05:14 -0800

    added portfolio as a portfolio item... very meta!

---
+++ src/components/MetaPortfolioWelcome.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ComponentContainer from "./ComponentContainer";
+import linkedInSeattle from "./photos/linkedin-Seattle.jpg";
+import seattleDay from "./photos/seattle-day-hi-res.jpg";
+import FirstHeader from "./FirstHeader";
+import github from "./photos/github-icon.png";
+import linkedin from "./photos/linkedin-icon.png";
+import emailIcon from "./photos/email-icon.png";
+import twitter from "./photos/twitter-icon.png";
+import portfolioArr from "../portfolio-items/portfolio-data";
+import { Link } from "react-router-dom";
+
+export default function MetaPortfolioWelcome(props) {
+  return (
+    <ComponentContainer id="top" darkModeCont={props.darkModeApp.darkMode}>
+      <FirstHeader
+        text="Easter Egg Alert!"
+        subtext="You found a hidden page!"
+        darkModeHeader={props.darkModeApp.darkMode}
+      />
+      <h1>
+        Congratulations! You managed to find this hidden page, either by
+        clicking on the "deployed" link for my portfolio or typing the url
+        directly in the url bar. Realistically, there isn't a need to click on
+        the deployed link for the portfolio project, since that's where you
+        already were, but I'm glad you're here regardless.{" "}
+      </h1>
+      <br />
+      <br />
+      <h1>
+        I would love to hear from you with the contact form below. There's
+        actually ANOTHER easter egg within the contact form if you feel up to
+        the challenge (or maybe you've already found that one too)! archive{" "}
+        <Link to="/archive">
+          <span className="hover:underline text-dclpal1-300">here</span>
+        </Link>
+        , and really track the evolution of my work.
+      </h1>
+    </ComponentContainer>
+  );
+}
---
+++ src/portfolio-items/portfolio.js
@@ -0,0 +1,18 @@
+import ReactionTestIMG from "../components/photos/portfolio-photos/reactiontest.JPG";
+
+const portfolio = {
+  name: "Portfolio",
+  dataProject: "portfolio",
+  image: ReactionTestIMG,
+  headline: "This portfolio! ... although, it IS a little meta.",
+  repo: "https://github.com/dionleung14/portfolio-nodemailer",
+  deployed: "https://dionleung.engineer/meta-portfolio/",
+  description: [
+    "This portfolio is built in React.js, and features a lot of different components.",
+    "Feel free to send the link to your friends (or enemies) to get their hearts racing for a little bit! If you enter 'charizard' as the password, you will bypass the jumpscare and be able to complete the reaction test without worrying about the jump scare.",
+    "I used scoped styling for each .vue component, which helped me practice my vanilla CSS.",
+  ],
+  collaborators: [],
+};
+
+export default portfolio;
