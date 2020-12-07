c3bc2defa0601c61fe4de723e5503a1712e5fabd
NA Flipper <dioncleung@gmail.com>
2020-12-07 00:12:00 -0800

    fixed const bug

--- src/components/PortfolioBootcamp.js
+++ src/components/PortfolioBootcamp.js
@@ -37,8 +37,6 @@ export default function PortfolioBootcamp(props) {
   //   console.log("YESSSSSSSSSIRRRRRRRR");
   // };
 
-  const { bootcampArr } = allArrays;
-
   const [portfolioItem, setPortfolioItem] = useState({
     item: "",
   });
